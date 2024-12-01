46085091e3e1d336f400009ef44c8abb4a9ce7fd
DioDev <diogo.m.raposo@gmail.com>
2024-12-01 23:36:49 +0000

    type shi

--- include/utils.h
+++ include/utils.h
@@ -12,5 +12,7 @@ void validate_input(int M, int N, Location* citizens, int num_citizens,
                    Location* supermarkets, int num_supermarkets);
 void print_test_results(const char* test_name, int total_time, int num_processes,
                        Solution* solution, int iterations, int time_to_best);
+void write_file_results(const char* filename, int total_time, Solution* solution, 
+                       int iterations, int time_to_best);
 
 #endif
